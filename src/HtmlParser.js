94e739fd6c0b9f9f0308874be2d6db4310f78def
Gawain Hammond <gawain@mu.local>
2016-10-13 18:44:32 +0100

    added html parser for extracting html hyper links

--- src/HtmlParser.js
+++ src/HtmlParser.js
@@ -1,20 +1,48 @@
 var cheerio = require('cheerio');
+var async = require('async');
 
-module.exports.extractLinks = function(html) {
+/**
+ * Parse HTML data and return linked resources for web crawling
+ *
+ */
+module.exports.extractLinks = function(html, clientCallback) {
+
+  var $dom = cheerio.load(html);
   var links = [];
-  var $ = cheerio.load(html);
-  var hrefs = $('a');
-  for (var i = 0 ; i < hrefs.length ; i++) {
-      var link = hrefs[i].attribs.href;
-      if (isInternalLink(link)) {
-        links.push(link);
+  async.parallel([
+      function(asyncCallback) {
+        parseDom($dom, {ele: 'a', attrib: 'href'}, links, asyncCallback);
+      },
+      function(asyncCallback) {
+        parseDom($dom, {ele: 'script', attrib: 'src'}, links, asyncCallback);
+      },
+      function(asyncCallback) {
+        parseDom($dom, {ele: 'img', attrib: 'src'}, links, asyncCallback);
       }
-  }
-  return links;
+  ], function(err, results) {
+      clientCallback(links);
+  });
 }
 
+function parseDom($dom, params, links, callback) {
+  var numOfElements =  $dom(params.ele).length;
+  if (numOfElements <= 0) callback();
+  $dom(params.ele).each(function(index, ele) {
+  var link = $dom(ele).attr(params.attrib);
+  if (isInternalLink(link)) {
+    links.push(link);
+  }
+  if (index ==  numOfElements -1) {
+    callback();
+  }
+});
+}
+
+
+
+
 function isInternalLink(hyperlink) {
-  if (hyperlink && hyperlink.indexOf('/') === 0 && hyperlink.indexOf('http') !== 0) {
+  if (hyperlink && hyperlink.indexOf('/') === 0) {
     return true;
   }
   return false;
--- test/HtmlParserTest.js
+++ test/HtmlParserTest.js
@@ -3,28 +3,51 @@ var chai = require('chai')
 var expect = chai.expect
 var should = chai.should();
 var htmlParser = require('../src/HtmlParser.js');
-
-
-
-var chai = require('chai')
-var expect = chai.expect
-var should = chai.should();
-var UrlCrawler = require('../src/UrlCrawler.js');
-var superagent = require('superagent');
-var sinon = require('sinon');
-
-
+var fs = require('fs');
 
 
 
 describe('HtmlParserTest.js Tests: ', function() {
+  
+    it('extracts multiple <a href> hyperlinks', function(done) {
+        loadHtml('three-hyperlinks.html', function(html) {
+          htmlParser.extractLinks(html, function(links) {
+            expect(links).to.have.lengthOf(3);
+            expect(links).to.include('/l1');
+            expect(links).to.include('/l2');
+            expect(links).to.include('/l3');
+            done();
+          });
+        });
 
-    it('extracts <a href> hyperlinks', function() {
-        var html = '<html> <head> </head> <body>  <a href="/l1">link1<a> <a href="/l2">link2<a> <a href="/l3">link3<a> </body> </html>';
-        var links = htmlParser.extractLinks(html);
-        expect(links).to.have.lengthOf(3);
-        expect(links).to.include('/l1');
-        expect(links).to.include('/l2');
-        expect(links).to.include('/l3');
+    });
+
+    it('extracts multiple resource type links', function(done) {
+        loadHtml('multi-resource-links.html', function(html) {
+          htmlParser.extractLinks(html, function(links) {
+            expect(links).to.have.lengthOf(9);
+            expect(links).to.include('/l1');
+            expect(links).to.include('/l2');
+            expect(links).to.include('/l3');
+            expect(links).to.include('/js/script1.js');
+            expect(links).to.include('/js/script2.js');
+            expect(links).to.include('/js/script3.js');
+            expect(links).to.include('/img/logo-sml.png');
+            expect(links).to.include('/img/logo-med.png');
+            expect(links).to.include('/img/logo-lrg.png');
+            done();
+          });
+
+        });
     });
 });
+
+
+function loadHtml(filename, callback) {
+  fs.readFile(__dirname + '/../test-resources/html/' + filename, function (err, data) {
+  if (err) {
+    throw err;
+  }
+  callback(data);
+  });
+}
